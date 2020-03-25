053b34c4d4034edf175f126dde09e4e9aba6bebc
Chas Fricke <chas.fricke@vangst.com>
2020-03-24 18:02:53 -0600

    working on TextField filter logic

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components/macro";
+import TextField from "@material-ui/core/TextField";
 import Card from "../Card";
 
 const CardList = () => {
   const [hasError, setError] = useState(false);
-  const [pokemonList, setPokemonList] = useState([]);
+  const [completePokemonList, setCompletePokemonList] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState("");
+  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -13,23 +16,34 @@ const CardList = () => {
       );
       res
         .json()
-        .then(res => setPokemonList(res.pokemon))
+        .then(res => setCompletePokemonList(res.pokemon))
         .catch(err => setError(err));
     }
 
     fetchData();
   });
 
-  const GetList = list => {
-    if (list) {
-      return list.map(data => <Card key={data.id} cardData={data} />);
-    }
+  const startNewSearch = value => {
+    const newSearchList = completePokemonList.filter(pokemon =>
+      pokemon.name.includes(value)
+    );
+    setFilteredList(newSearchList);
+  };
+
+  const handleSearchTermChange = event => {
+    // setSearchTerm(event.target.value);
+    startNewSearch(event.target.value);
   };
 
   return (
     <>
       {hasError && JSON.stringify(hasError)}
-      <ListContainer>{GetList(pokemonList)}</ListContainer>
+      <TextField onChange={handleSearchTermChange} />
+      <ListContainer>
+        {filteredList.map(data => (
+          <Card key={data.id} cardData={data} />
+        ))}
+      </ListContainer>
     </>
   );
 };
@@ -40,4 +54,6 @@ const ListContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 48px;
 `;
